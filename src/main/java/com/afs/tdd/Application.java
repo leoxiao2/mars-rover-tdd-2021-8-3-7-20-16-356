d03a5aafd7c87cc69ba1d3ef222a28f93908ec43
Leo xiao <leo.xiao@oocl.com>
2022-07-21 16:07:56 +0800

    test: should_turn_left_when_ExecuteSingleCommand_given_left_command

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,4 +1,33 @@
 package com.afs.tdd;
 
+import java.lang.reflect.Executable;
+import java.util.List;
+
 public class Application {
+    private int locationX;
+    private int locationY;
+    private  String direction;
+    public Application(int locationX,int locationY,String direction){
+        this.locationX=locationX;
+        this.locationY=locationY;
+        this.direction=direction;
+    }
+
+    public void ExecuteSingleCommand(String command){
+    }
+
+    public void ExecuteCommands(List<String> commands){
+    }
+
+    public String getDirection() {
+        return this.direction;
+    }
+
+    public int getLocationX(){
+        return  this.locationX;
+    }
+
+    public int getLocationY(){
+        return this.locationY;
+    }
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,17 @@
 package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void write_your_first_test() {
-
+    void should_turn_left_when_ExecuteSingleCommand_given_left_command() {
+        //given
+        Application application=new Application(0,0,"N");
+        //when
+        application.ExecuteSingleCommand("L");
+        //then
+        assertEquals("W",application.getDirection());
     }
+    
 }
