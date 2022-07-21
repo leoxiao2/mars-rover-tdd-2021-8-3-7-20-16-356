95bfa35fb3f5caec70f4b1a6947bf7b973129869
Leo xiao <leo.xiao@oocl.com>
2022-07-21 16:26:16 +0800

    test: should_go_to_0_0_W_when_execute_command_L_given_0_0_N

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -15,5 +15,17 @@ class MarsRoverTest {
         assertEquals(1,marsRover.getLocationY());
         assertEquals("N",marsRover.getDirection());
     }
+
+    @Test
+    void should_go_to_0_0_W_when_execute_command_L_given_0_0_N() {
+        //given
+        MarsRover marsRover=new MarsRover(0,0,"N");
+        //when
+        marsRover.ExecuteSingleCommand("L");
+        //then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("W",marsRover.getDirection());
+    }
     
 }
